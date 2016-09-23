374ac4d245b588a698629f0970480cf1e03d0192
Francis <FrancisPBaker@gmail.com>
2016-09-23 17:52:04 -0400

    Assignment #2
    This is my second assignment for this course.

---
+++ Assignment #2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std; 
+
+
+int main()
+{
+	int var1 = 0 , var2 = 0, var3 = 0;
+	cin >> var1 >> var2 >> var3;
+	
+	cout << var1 << " " << var2 << " " << var3;
+	return 0;
+	}
