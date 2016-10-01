505690721d9fb289d480da7add27ab9438ee2572
Francis <FrancisPBaker@gmail.com>
2016-10-01 01:04:01 -0400

    Assignment #2
    This is my third assignment  for this class.

---
+++ Assignment #3/Assignment 3/Assignment 3/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+double a;
+
+	int main()
+	{
+		cout << "enter a decimals: " ;
+		cin >> a;
+		return a;
+
+	}
+	int nodecimal()
+	{
+		int intergerPart = (int)a;
+		return a;
+	}
